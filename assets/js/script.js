cf676cc77cf33e23c393c383ebbfbcf5bc97dfc9
apisdriven <apisdriven@gmail.com>
2022-10-24 20:09:12 -0400

    added modal screen, CSS & Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,25 @@
-function searchApi(query, format) {
-    var locQueryUrl = 'https://AmI7oSNWUm61D2MFJwKvY6ql2t46Ld7oucWadgCQkphEjOuV-FqFHrU0d1stfOO2/?fo=json';
-  
-    if (format) {
-      locQueryUrl = 'https://AmI7oSNWUm61D2MFJwKvY6ql2t46Ld7oucWadgCQkphEjOuV-FqFHrU0d1stfOO2/' + format + '/?fo=json';
-    }
-  
-    locQueryUrl = locQueryUrl + '&q=' + query;
-  
-    fetch(locQueryUrl)
-      .then(function (response) {
-        if (!response.ok) {
-          throw response.json();
-        }
-  
-        return response.json();
-      })
-      .then(function (locRes) {
-        // write query to page so user knows what they are viewing
-        resultTextEl.textContent = locRes.search.query;
-  
-        console.log(locRes);
-  
-        if (!locRes.results.length) {
-          console.log('No results found!');
-          resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
-        } else {
-          resultContentEl.textContent = '';
-          for (var i = 0; i < locRes.results.length; i++) {
-            printResults(locRes.results[i]);
-          }
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
